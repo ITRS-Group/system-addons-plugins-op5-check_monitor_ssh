34750bbaa20727f91a1f3fef63db09a82c8e1957
Johan Thoren <johan@thoren.xyz>
2021-08-25 11:34:07 +0200

    Version 0.1.12

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject check-monitor-ssh "0.1.12-SNAPSHOT"
+(defproject check-monitor-ssh "0.1.12"
   :description "Check Monitor SSH"
   :url "https://itrsgroup.com"
   :license {:name "GPL-3.0"
